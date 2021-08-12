ce2b478320fe6804c110c6fb468bdf3381f93b13
hsierra37 <hernando.sierra@tecwis.com>
2021-08-12 18:09:17 -0400

    revert SEO change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Cloud Computing",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "http://0.0.0.0:4000/services/cloud_computing/",
+        "url": "/services/cloud_computing/",
         "teaser": null
       },{
         "title": "Web Development",
         "excerpt":"Here is a sample service collection   I can describe the service here !!!!  ","categories": [],
         "tags": [],
-        "url": "http://0.0.0.0:4000/services/development/",
+        "url": "/services/development/",
         "teaser": null
       }]
